e8b41f44e06542d8ddf5eee72841a109e9acb603
github username <github email address>
2018-12-30 18:37:18 -0600

    validation updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :bookings, class_name: "Event", foreign_key: "friend_user_id"
   has_many :activities, through: :events
   has_many :locations, through: :events
-
+  validates :gender, inclusion: { in: %w(male female),
+    message: "Please enter male or female for gender!" }
   validates :phone, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, message: "bad format" }
-  #validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  #validates :gender, inclusion: { in: %w(male female),
-  #  message: "Please enter male or female for gender!" }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
 end
