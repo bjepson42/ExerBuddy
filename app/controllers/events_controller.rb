789b09025101c01588ae813fc9bf6066ef5357bd
github username <github email address>
2019-01-02 13:24:31 -0500

    user login works

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :date, :user_id, :friend_user_id, :location_id, :activity_id)
+      params.require(:event).permit(:name, :date, :user_id, :location_id, :activity_id, :event_paired)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,5 @@ class Event < ApplicationRecord
  belongs_to :location
  belongs_to :activity
  belongs_to :user
- belongs_to :friend_user, class_name: "User"
-
+ belongs_to :friend_user, class_name: "User", required: false
 end
