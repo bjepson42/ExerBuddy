34a80d7e482ffcd41e688cf58920745f3075fa99
github username <github email address>
2019-01-02 18:02:12 -0500

    user creation now logs in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+    else
+      render :new
+    end
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
