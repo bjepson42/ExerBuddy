5c94f182a3ab476861e37b7d1e0e5b093f5d3ea2
github username <github email address>
2019-01-02 14:21:22 -0500

    small user form validation additions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   validates :phone, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, message: "bad format" }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :age, :numericality => { greater_than_or_equal_to: 18, less_than: 100, message: "You must be older than 18." }
 end
