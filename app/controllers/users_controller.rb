e8c0857d053516447ed8cb4a3182913d1fb3d033
github username <github email address>
2019-01-03 18:51:57 -0500

    profile pic uploader added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,6 +68,7 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -77,6 +78,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:comment, :username, :name, :age, :gender, :phone, :email, :password, :password_confirmation)
+      params.require(:user).permit(:comment, :username, :name, :age, :gender, :phone, :email, :password, :password_confirmation, :image)
     end
 end
