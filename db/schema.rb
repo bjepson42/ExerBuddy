6467a9c5521eb28b3b4196b211fe4911853e14cb
github username <github email address>
2019-01-03 19:48:16 -0500

    clean up tables, more add user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2019_01_03_233657) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "average_caches", force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "avg", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_average_caches_on_rateable_type_and_rateable_id"
+    t.index ["rater_id"], name: "index_average_caches_on_rater_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
@@ -59,6 +70,27 @@ ActiveRecord::Schema.define(version: 2019_01_03_233657) do
     t.string "description"
   end
 
+  create_table "overall_averages", force: :cascade do |t|
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "overall_avg", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_overall_averages_on_rateable_type_and_rateable_id"
+  end
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.string "cacheable_type"
+    t.integer "cacheable_id"
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+    t.index ["cacheable_type", "cacheable_id"], name: "index_rating_caches_on_cacheable_type_and_cacheable_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -70,7 +102,6 @@ ActiveRecord::Schema.define(version: 2019_01_03_233657) do
     t.string "password_digest"
     t.string "username"
     t.string "comment"
-    t.string "image"
   end
 
 end
